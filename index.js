550d0af4a35da1eb99b47e250ed7afb21dc05125
bill <bill.arnold@student.moringaschool.com>
2023-10-04 01:20:52 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+//The receivesAFunction function
+ function MondayRoutine(){
+     console.log("Eat Breakfast And Exercise")
+ }
+
+//returnsANamedFunction
+function DailyMenu(Nutrution){
+
+    let meal;
+
+     if (Nutrition === protein){
+         BreakFast = "steak";
+         console.log ("yummy")
+      } else if (Nutrition === vitamins){
+           BreakFast ="orange"
+           console.log ("nice")
+    }else {
+        BreakFast = "cereal"
+    }
+
+
+DailyMenu(Nutrution);
+
+return function (){
+    console.log(DailyMenu)
+} 
+}
+//returnsAnAnonymousFunction
+function names(){
+
+}
+
+names;
